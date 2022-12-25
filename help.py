98f71eef9e75776e1793faf868c0bfc35c2237ba
maquezada88 <107223686+maquezada88@users.noreply.github.com>
2022-12-25 12:27:03 -0500

    Update help.py

--- help.py
+++ help.py
@@ -1,10 +1,45 @@
 import random
+import json
 
 def usage():
     print("Use addition, multiplication, subtraction, or division in order to get 24!")
+    print("Can only use each number once!")
 
-def rand_num():
-    return random(0,3)
+def get_set_of_numbers(difficulty):
+    
+
+    if difficulty == 1:
+        nums = get_easy_numbers()
+        return nums
+    elif difficulty == 2:
+        nums = get_medium_numbers()
+        return nums
+    elif difficulty ==3:
+        nums = get_hard_numbers()
+        return nums
+    else:
+        print("ERROR IN GET SET OF NUMBERS")
+
+def get_easy_numbers():
+    x = random.randint(1, 5) - 1
+    f = open('easy_numbers.json')
+    data = json.load(f)
+    easy_nums = data['easy'][x]
+    return easy_nums
+
+def get_medium_numbers(x):
+    x = random.randint(1, 5) - 1
+    f = open('medium_numbers.json')
+    data = json.load(f)
+    medium_nums = data['easy'][x]
+    return medium_nums
+
+def get_hard_numbers(x):
+    x = random.randint(1, 5) - 1
+    f = open('hard_numbers.json')
+    data = json.load(f)
+    hard_nums = data['easy'][x]
+    return hard_nums
 
 def validate_difficulty(difficulty):
     if int(difficulty) < 1 or int(difficulty) > 3:
